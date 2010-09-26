ff94907aa0b976f8d93949a2388a18b3dfa43625
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-09-26 20:07:55 +0400

    Implment blog posts listing on home page.

---
+++ media/js/recentposts.js
@@ -0,0 +1,28 @@
+var numposts = 10;
+
+function recentPosts(json) {
+	for (var i = 0; i < numposts; i++) {
+		var post = json.feed.entry[i];
+		var postTitle = post.title.$t;
+		var tags = '';
+		var postLink;
+		var j = 0;
+
+		// create command-separated link of tags
+		while (j < post.category.length - 1) {
+			tags += post.category[j].term + ", ";
+			j++;
+		}
+		tags += post.category[j].term;
+
+		// fetch post link
+		for (var k = 0; k < post.link.length; k++) {
+			if (post.link[k].rel == 'alternate') {
+				postLink = post.link[k].href;
+				break;
+			}
+		}
+
+		$("<li><a href='" + postLink + "'>" + postTitle + "</a> (" + tags + ")</li>").appendTo("#envlist");
+	}
+}
